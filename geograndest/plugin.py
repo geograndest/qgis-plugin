231f96f2642593e300b2345e896b8b281e05ca8b
arichard-geograndest <61828327+arichard-geograndest@users.noreply.github.com>
2022-01-27 15:59:55 +0100

    Update plugin.py

--- geograndest/plugin.py
+++ geograndest/plugin.py
@@ -5,12 +5,12 @@ from qgis.PyQt.QtCore import Qt
 
 import os.path
 
-from geograndest.utils.plugin_globals import PluginGlobals
-from geograndest.gui.dock import DockWidget
-from geograndest.gui.about_box import AboutBox
-from geograndest.gui.param_box import ParamBox
-from geograndest.nodes.tree_node_factory import TreeNodeFactory
-from geograndest.nodes.tree_node_factory import download_tree_config_file
+from datagrandest.utils.plugin_globals import PluginGlobals
+from datagrandest.gui.dock import DockWidget
+from datagrandest.gui.about_box import AboutBox
+from datagrandest.gui.param_box import ParamBox
+from datagrandest.nodes.tree_node_factory import TreeNodeFactory
+from datagrandest.nodes.tree_node_factory import download_tree_config_file
 
 
 class SimpleAccessPlugin:
@@ -68,8 +68,7 @@ class SimpleAccessPlugin:
         Creates the plugin main menu
         """
         plugin_menu = self.iface.pluginMenu()
-        ##self.plugin_menu = QMenu(u"Géo2France", plugin_menu)
-        self.plugin_menu = QMenu(u"GéoGrandEst", plugin_menu)
+        self.plugin_menu = QMenu(u"DataGrandEst", plugin_menu)
         plugin_menu.addMenu(self.plugin_menu)
 
         show_panel_action = QAction(u'Afficher le panneau latéral', self.iface.mainWindow())
