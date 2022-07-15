a01b7f75b1be5de02373b43f1ef470910f8a742e
Benjamin C <2361255+bchartier@users.noreply.github.com>
2022-07-15 09:19:11 +0200

    style: fix typo in aboutbox

--- geograndest/gui/about_box.py
+++ geograndest/gui/about_box.py
@@ -26,7 +26,7 @@ class AboutBox(QDialog):
         title = u"À propos de l'extension DataGrandEst…"
         description1 = """Extension pour QGIS donnant un accès simplifié aux ressources géographiques de <a href="https://www.datagrandest.fr/">DataGrandEst</a> Version {}<br>""".format(PluginGlobals.instance().PLUGIN_VERSION)
         description2 = """Plus d'informations à l'adresse suivante :<br><a href='{0}'>{0}</a><br>""".format(PluginGlobals.instance().PLUGIN_SOURCE_REPOSITORY)
-        description3 = """Merci aux créateurs des plugins <a href="https://github.com/geo2france/idg-qgis3-plugin">Géo2France </a>, <a href="contact@bchartier.net">Benjamin CHARTIER</a>, <a href="https://github.com/geobretagne/qgis-plugin">Géobretagne</a> et <a href="https://gitlab.in2p3.fr/letg/indigeo-for-qgis">Indigéo </a>sur lesquels ce plugin est basé !"""
+        description3 = """Merci aux créateurs des plugins <a href="https://github.com/geo2france/idg-qgis3-plugin">Géo2France </a>, <a href="contact@bchartier.net">Benjamin CHARTIER</a>, <a href="https://github.com/geobretagne/qgis-plugin">GéoBretagne</a> et <a href="https://gitlab.in2p3.fr/letg/indigeo-for-qgis">Indigéo </a>sur lesquels ce plugin est basé !"""
 
         self.textArea = QTextBrowser()
 #        self.textArea.setTextInteractionFlags(Qt.LinksAccessibleByMouse)
